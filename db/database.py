90b4687467f1cd90df8bee43abacb3e565748095
AyazYousafxai <ayazpk6630@gmail.com>
2023-11-28 12:43:06 +0500

    remove  Secret type: PostgreSQL URI

--- db/database.py
+++ db/database.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}/{settings.POSTGRES_DB}"
-# "postgres://tjupcnmt:dUfYgolsLVBXiZ1GjB9yORiPjC-jnBQb@floppy.db.elephantsql.com/tjupcnmt"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
